7e03ca6f1971689dad545568b1d8ebbe88ce5a30
Diego Goyret <diegoyret@gmail.com>
2016-06-09 11:07:20 -0500

    final version

--- app/models/bloque.rb
+++ app/models/bloque.rb
@@ -1,5 +1,5 @@
 class Bloque < ActiveRecord::Base
 has_many :tasks,:class_name => "Task", :foreign_key => "bloque_id"
-has_many :submissions,:class_name => "Submission", :foreign_key => "user_id"
+# has_many :submissions,:class_name => "Submission", :foreign_key => "user_id"
 
 end
